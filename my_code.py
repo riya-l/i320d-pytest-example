a23858363d51bfa61df408af11840b82e87b3c71
riya-l <123594164+riya-l@users.noreply.github.com>
2024-04-03 16:35:48 -0500

    Update my_code.py

--- my_code.py
+++ my_code.py
@@ -14,3 +14,7 @@ def test_fix_phone_num():
   assert fix_phone_num("5125558823") == '(512) 555 8823'
   assert fix_phone_num("5554429876") == '(555) 442 9876'
   assert fix_phone_num("3216543333") == '(321) 654 3333'
+
+def text_fix_dash_and_parentheses():
+  assert fix_phone_num("555-442-98761") == '(555) 442 98761'
+  assert fix_phone_num("(321) 654 3333") == '(321) 654 3333'
